f8d049860995bc679244f8b9470e57eee923b488
Juan Gallardo <00091813@uca.edu.sv>
2020-04-17 18:52:09 -0600

    Modify: main && Add: OutOfRangeException

--- src/com/JHGH/x00091813/Main.java
+++ src/com/JHGH/x00091813/Main.java
@@ -5,6 +5,41 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        Empresa business = new Empresa("IBM");
+        int menuChoice = 0;
+        do {
+            try {
+                menuChoice = mainMenu();
+                if (menuChoice < 1 || menuChoice > 6)
+                    throw new OutOfRangeException("Esa opcion no existe.");
+            }
+            catch (OutOfRangeException ex){
+                JOptionPane.showMessageDialog(null, ex);
+                menuChoice = 0;
+            }
+            switch (menuChoice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        } while (menuChoice != 6);
+    }
 
+    public static int mainMenu() {
+        int x = 0;
+        try {
+            x = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "MENU:\n1) Agregar empleado\n2) Despedir empleado\n3) Ver lista de empleados\n" +
+                            "4) Calcular sueldo\n5) Mostrar totales\n6) SALIR"));
+        }
+        catch (IllegalArgumentException ex) {}
+        return x;
     }
 }
---
+++ src/com/JHGH/x00091813/OutOfRangeException.java
@@ -0,0 +1,7 @@
+package com.JHGH.x00091813;
+
+public class OutOfRangeException extends Exception{
+    public OutOfRangeException(String message) {
+        super(message);
+    }
+}
